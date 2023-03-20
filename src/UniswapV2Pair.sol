cbeb84e886cef819246f38d82817fb2e7b454262
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-20 19:49:00 +0100

    totalSupply==0

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "solmate/tokens/ERC20.sol";
+import "./librairies/Math.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -10,9 +11,26 @@ interface IERC20 {
     function transfer(address to, uint256 amount) external;
 }
 
-contract UniswapV2Pair is ERC20("UniswapV2", "UNIV2", 18) {
-    uint256 private reserve0;
-    uint256 private reserve1;
+contract UniswapV2Pair is ERC20("UniswapV2", "UNIV2", 18), Math {
+    uint256 constant MINIMUM_LIQUIDITY = 1000;
 
-    function mint() external {}
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+
+    function mint() external {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
+
+        uint256 liquidity;
+
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        }
+    }
 }
