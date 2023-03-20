1ff455d75217b0bc2b1b604f7460b63ce9c968f3
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-20 19:12:25 +0100

    interface

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "solmate/tokens/ERC20.sol";
 
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
+
 contract UniswapV2Pair is ERC20("UniswapV2", "UNIV2", 18) {
     uint256 private reserve0;
     uint256 private reserve1;
+
+    function mint() external {}
 }
