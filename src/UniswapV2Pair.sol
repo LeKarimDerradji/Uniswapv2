8ba852da36efa2f3a13d4bef19ecd56af46141bd
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-20 18:28:26 +0100

    commit

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "solmate/tokens/ERC20.sol";
 
-contract UniswapV2Pair is ERC20  {
+contract UniswapV2Pair is ERC20("UniswapV2", "UNIV2", 18) {
     uint256 private reserve0;
     uint256 private reserve1;
-    
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/UniswapV2Pair.sol";
+import {UniswapV2Pair} from "../src/UniswapV2Pair.sol";
 
-contract UniswapV2Pair is Test {
+contract UniswapV2PairTest is Test {
     UniswapV2Pair public uniswapv2pair;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        //uniswapv2pair = new UniswapV2Pair();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function testIncrement() public {}
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    function testSetNumber(uint256 x) public {}
 }
